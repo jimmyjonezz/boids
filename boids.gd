4408123ff442b407b4cd6ac3827b8ab304e13453
jimmyjonezz <jimmyjonezz@bk.ru>
2019-06-11 21:31:44 +0300

    0.1.2

--- boids.gd
+++ boids.gd
@@ -1,19 +1,20 @@
 extends KinematicBody2D
 
 var velocity = Vector2.ZERO
-var max_velocity = 100
+var max_velocity = 4
 var destination = Vector2.ZERO
+var FOLLOW_SPEED = 2
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
-	var vector = (destination - position).normalized()
-	velocity = vector * max_velocity
-	move_and_collide(velocity * delta)
+	var vector = (destination - position).normalized() * max_velocity
+	velocity = velocity.linear_interpolate(vector, delta * FOLLOW_SPEED)
+	move_and_collide(velocity)
 	
-	print("destination: %s, pos: %s, vec: %s, vel: %s" % 
-			[destination, position, vector, velocity])
+	#print("destination: %s, pos: %s, vec: %s, vel: %s" % 
+	#		[destination, position, vector, velocity])
 
 func _on_timer_timeout():
 	destination = Vector2(randf() * get_viewport_rect().size.x,
