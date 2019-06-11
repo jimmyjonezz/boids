d84ddc028bcaca5b2cc583b24966b182dcb7126e
jimmyjonezz <jimmyjonezz@bk.ru>
2019-06-11 19:08:26 +0300

    0.1
    var velocity = Vector2.ZERO
    var max_velocity = 100
    
    func _ready():
    	pass
    
    func _physics_process(delta):
    	velocity = (get_global_mouse_position() - position).normalized() * max_velocity
    	move_and_collide(velocity * delta)

--- boids.gd
+++ boids.gd
@@ -1,7 +1,11 @@
 extends KinematicBody2D
 
+var velocity = Vector2.ZERO
+var max_velocity = 100
+
 func _ready():
 	pass
 	
 func _physics_process(delta):
-	pass
+	velocity = (get_global_mouse_position() - position).normalized() * max_velocity
+	move_and_collide(velocity * delta)
